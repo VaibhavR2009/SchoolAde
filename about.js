16233b95d91b4bdecb7c451c0380436773b758f9
VaibhavR2009 <vaibhav.ramji09@gmail.com>
2024-08-09 14:31:35 -0400

    Update about.js

--- about.js
+++ about.js
@@ -2,3 +2,7 @@ let back = document.getElementById("back");
 back.addEventListener("click", ()=>{
     window.location.href = "app.html";
 })
+window.addEventListener('beforeunload', function (e) {
+    e.preventDefault();
+    window.location.href="app.html";
+});
